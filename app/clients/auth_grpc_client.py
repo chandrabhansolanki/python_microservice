66235db1a62f9a76cc2192ea047b2c03798cf0eb
chandrabhansingh <chandrabhan.singh@sarvika.com>
2026-02-23 01:17:23 +0530

    check: checking pipline auto matically start or not

--- app/clients/auth_grpc_client.py
+++ app/clients/auth_grpc_client.py
@@ -3,6 +3,7 @@ from proto import auth_pb2, auth_pb2_grpc
 
 def grpc_login(email, password):
     print("➡️ HTTP → gRPC CALL")
+    print("➡️ HTTP → gRPC CALL")
 
     channel = grpc.insecure_channel("localhost:50051")
     stub = auth_pb2_grpc.AuthServiceStub(channel)
